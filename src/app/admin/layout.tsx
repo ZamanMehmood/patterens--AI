c9385339f91ff4c05ee3a34d3581c307bbcfa6f1
zaman <zaman342003@gmail.com>
2025-10-16 00:16:59 +0500

    sideba done

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -6,8 +6,8 @@ export default function AdminLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="min-h-screen bg-white text-gray-900">
-      <header className="p-4 bg-blue-600 text-white text-center font-semibold">
+    <div className="min-h-screen  text-dark-900" style={{backgroundColor:"2F2F2F"}}>
+      <header className="p-4 text-dark text-start font-semibold"  >
         Admin Portal
       </header>
       <main className="p-6">{children}</main>
